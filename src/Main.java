aa89a75d42c1d217caf4556f70f3d96fea4c7640
Lana L <lana.shogo@gmail.com>
2022-07-03 17:23:52 +0300

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 500;
+        int bonusMile = 20;
+
+        int totalBonusMiles = ticketPrice / bonusMile;
+
+        System.out.println("Ваши бонус мили: " + totalBonusMiles);
+    }
+}
